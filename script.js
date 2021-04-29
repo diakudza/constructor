a9bc830fd5ff0ec2cb90118be9d5944152bab311
diakudza <diakudza@gmail.com>
2021-04-29 17:45:37 +0300

    Подтягивание переменных в шапку программы

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
 let textArea = document.getElementById('textArea');
+let textVariables = document.getElementById('textVariables');
+
 let divWithBtn = document.getElementById('divWithBtn');
 
-function strToText(n) {
-    var arr = blocks[n].split(';');
-    return arr.join('\n');
-}
+
 let goEgit = {
     start: 0,
     generate() { //метод запуска рендера кнопок
@@ -14,20 +13,32 @@ let goEgit = {
             document.getElementById('generate').remove();
 
 
-        }
+        };
+        generateStandartVar();
+    }
+}
+function generateStandartVar() {
+    for (let variables of standart) {
+        textVariables.innerHTML += variables + '\n';
     }
 }
 
 function generateBtn() {
     for (let oper in blocks) {
         let btn = document.createElement('button');
-        btn.id = oper;
-        btn.innerText = oper;
+        btn.id = oper
+        btn.innerText = blocks[oper].item;
         btn.onclick = btnDo;
         divWithBtn.append(btn);
     }
 }
 
 function btnDo(e) {
-    textArea.innerHTML += strToText(e.target.id);
+    textArea.innerHTML += strToText(e.target.id, 'code');
+    textVariables.innerHTML += strToText(e.target.id, 'variables');
 };
+
+function strToText(n, type) {
+    let arr = blocks[n][type].split(';');
+    return arr.join('\n');
+}
