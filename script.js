44303c2b1a1b342824e6ae0b4f4e3c50f0e6d4f8
diakudza <diakudza@gmail.com>
2021-05-09 19:34:58 +0300

    Норм поменял

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let divBlocks = document.getElementsByClassName('divBlocks')[0];
 class Oper {
 
     constructor(n) {
-        this.name = n;
+        this.item = n;
         this.title = blocks[n].item;
         this.code = blocks[n].code;
         this.variables = blocks[n].variables;
@@ -14,70 +14,109 @@ class Oper {
     showCode() {
         console.log(this.item.code);
     }
-    createBlock(item, type, classBlock = "block") {
+    createBlock(item = this.item, code = this.code, classBlock = "block") {
+        if (gO.hasCut == 1) {
+            alert('Вы уже добавил отрезку!');
+            return;
+        }
+        if (item == 'cut') {
+            if (gO.hasCut !== 1) {
+                gO.hasCut = 1;
+                classBlock = 'end';
+            }
+        }
         let fragment = new DocumentFragment(),
             p = document.createElement('p'),
             span = document.createElement("span"),
             textArea = document.createElement("textarea"),
             divWrapBlock = document.createElement("div"),
-            btn = document.createElement('button');
-        div = document.createElement("div");
+            btn = document.createElement('button'),
+            div = document.createElement("div");
         gO.blocks++;
-        divWrapBlock.id = n + 'Block' + gO.blocks;
+        divWrapBlock.id = item + 'Block' + gO.blocks;
         divWrapBlock.classList.add('block');
-        divWrapBlock.dataset.name = n;
+        divWrapBlock.dataset.name = item;
         divWrapBlock.classList.add(classBlock);
         textArea.classList.add('BlockTextArea');
         btn.onclick = gO.removeBlock;
         btn.classList.add('btnClose');
         btn.innerHTML = '&#10060;';
         div.classList.add('divLayout');
-        p.innerText = blocks[n].item;
-        if (n == 'protochka') {
+        p.innerText = blocks[item].item;
+        if (item == 'protochka') {
             let startBlock = document.createElement('textarea');
             let middleBlock = document.createElement('textarea');
             let endBlock = document.createElement('textarea');
             middleBlock.id = 'protochkaMiddleText';
-            startBlock.value = blocks[n].code.start.split(';').join('\n');
+            startBlock.value = blocks[item].code.start.split(';').join('\n');
             middleBlock.value = '';//blocks[n].code['1'].split(';').join('\n');
-            endBlock.value = blocks[n].code.end.split(';').join('\n');
-            div.append(btn, p, startBlock, createBtnForProtochka(), middleBlock, endBlock);
+            endBlock.value = blocks[item].code.end.split(';').join('\n');
+            div.append(btn, p, startBlock, protochka.createBtn(), middleBlock, endBlock);
             divWrapBlock.append(p, div);
             fragment.append(divWrapBlock);
-            return fragment;
+            divBlocks.append(fragment);
+            return;
         }
-        textArea.value = strToText(n, type);
-        if (n == 'standart') {
+        textArea.value = blocks[item].code.split(';').join('\n');
+        if (item == 'standart') {
             div.append(p, textArea);
         } else {
             div.append(btn, p, textArea);
         }
 
         divWrapBlock.append(p, div);
-        for (variable in blocks[n].variables) {
-            gO.addHeadVar(variable, blocks[n].variables[variable].value, blocks[n].variables[variable].comment);
+        for (let variable in blocks[item].variables) {
+            gO.addHeadVar(variable, blocks[item].variables[variable].value, blocks[item].variables[variable].comment);
         }
         fragment.append(divWrapBlock);
-        return fragment;
+        divBlocks.append(fragment);
     }
 
     createBtn() {
         let btn = document.createElement('button');
         btn.id = this.name;
         btn.innerText = this.title;
-        btn.onclick = gO.btnDo;
+        btn.addEventListener('click', () => { massOfOper[this.idArr].createBlock() });
         btn.dataset.idArr = this.idArr;
         divWithBtn.append(btn);
     }
     removeBlock() { }
     exportCodeInArr() { }
+
 }
 let massOfOper = [];
+let protochka = {
+};
 for (let oper in blocks) {
     massOfOper.push(new Oper(oper));
+    if (oper == 'protochka') {
+        protochka = new Oper(oper);
+    }
 }
-console.log(massOfOper);
+protochka.createBtn = function () {
 
+    let div = document.createElement('div');
+
+    div.classList.add('buttonsWrap');
+    for (kontur in blocks.protochka.code) {
+        let img = document.createElement('img');
+        if (kontur == 'start' || kontur == 'end') { continue; }
+        let btn = document.createElement('button');
+        btn.innerHTML = blocks.protochka.code[kontur].title;
+        btn.dataset.kont = blocks.protochka.code[kontur].id;
+        img.src = 'img/' + blocks.protochka.code[kontur].img;
+        btn.onclick = protochka.btnProtochka;
+        btn.classList.add('buttonsProtochka');
+        btn.append(img);
+        div.append(btn);
+
+    }
+    return div;
+}
+protochka.btnProtochka = function (e) {
+    let middleText = document.getElementById('protochkaMiddleText');
+    middleText.value += blocks.protochka.code[e.target.dataset.kont].code.split(';').join('\n');
+}
 let gO = {
     start: 0,
     blocks: 0,
@@ -89,18 +128,19 @@ let gO = {
             this.start = 1;
 
             for (let i of massOfOper) {
+                if (i.item == 'standart') { continue; }
                 i.createBtn();
             }
 
             let btnSave = document.createElement('a');
             btnSave.id = 'save';
             btnSave.innerText = 'Собрать файл';
-            btnSave.onclick = this.generateProg;
+            btnSave.onclick = gO.generateProg;
             divWithBtn.append(btnSave);
         };
 
-        divBlocks.append(gO.createHeadBlock());
-        // divBlocks.append(standart.createBlock('standart', 'code', 'head'));
+        divBlocks.append(gO.createHeadBlock());//выводим блок HEAD
+        massOfOper[massOfOper.length - 1].createBlock();//выводим блок Стандарт
     },
 
     /** Функция создает первый блок (шапку программы) */
@@ -144,30 +184,12 @@ let gO = {
         return fragment;
     },
 
-    btnDo(e) {
-        if (gO.hasCut == 1) {
-            alert('После отрезки блоки добавлять нельзя!');
-            return;
-        }
-        let textArea = document.getElementById('textArea');
-        let textVariables = document.getElementById('textVariables');
-        if (e.target.id == 'cut') {
-            if (gO.hasCut !== 1) {
-                gO.hasCut = 1;
-                divBlocks.append(createBlock(e.target.id, 'code', 'end'));
-            } else {
-                alert('Вы уже добавил отрезку!');
-            }
-        } else {
-            divBlocks.append(createBlock(e.target.id, 'code').bind(massOfOper[e.target.dataset.idArr]));
-        }
-    },
     /** склеиваем собранную прогрмамму в массив, и выводим в файл */
     generateProg() {
         existProg[0] = '', existProg[1] = '';
-        dlinaDetail();
-        getExistHead();
-        getBlockText();
+        gO.dlinaDetail();
+        gO.getExistHead();
+        gO.getBlockText();
         let myData = 'data:application/txt;charset=utf-8,' + encodeURIComponent(existProg[0] + existProg[1].split(';').join('\n'));
         this.href = myData;
         this.download = 'O0020.txt';
@@ -176,7 +198,7 @@ let gO = {
     removeBlock(e) {
         let block = e.target.parentNode;
         if (e.target.parentNode.parentNode.id.substr(0, 3) == 'cut') {
-            go.hasCut = 0;
+            gO.hasCut = 0;
         }
         let oper = e.target.parentNode.parentNode.dataset.name;
 
@@ -205,7 +227,35 @@ let gO = {
         let div = document.getElementById('headLayout');
         divWrap.append(spanVar, input, span);
         div.append(divWrap);
+    },
+    dlinaDetail() {
+        let typeM = document.getElementById('divWrap#1');
+        let diam = document.getElementById('divWrap#2');
+        let dlina = document.getElementById('divWrap#531');
+        // let razmerZag = typeM.children[1].value;
+        if (typeM.children[1].value == "1") {
+            dlina.children[1].value = diam.children[1].value * 1.155 + 1.0;
+        } else {
+            dlina.children[1].value = +diam.children[1].value + 1.0;
+        }
+    },
+    getExistHead() {
+        let arrOfVar = document.querySelectorAll('#headLayout > div');
+        for (item of arrOfVar) {
+            if (item.childNodes[1].name == 'name') {
+                existProg[0] += `${item.childNodes[1].value}\n`;
+                continue;
+            }
+            existProg[0] += `${item.childNodes[1].name}=${item.childNodes[1].value}${item.childNodes[2].innerHTML}\n`;
+        }
+    },
+    getBlockText() {
+        let div = document.querySelectorAll('.divBlocks > div:not(#head)');
+        for (item of div) {
+            existProg[1] += `${item.childNodes[1].childNodes[0].value}`;
+        }
     }
 }
+
 
 window.onload = gO.generate;
